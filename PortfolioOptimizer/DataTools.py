a1eaad275d0cf0cf6b884cb801c48f52b25556a5
msquaredds <alex.melesko@msquaredds.com>
2023-03-23 19:11:37 -0400

    Pulling data

--- PortfolioOptimizer/DataTools.py
+++ PortfolioOptimizer/DataTools.py
@@ -43,12 +43,14 @@ class DataTools(object):
                 price_data = gcp_engine.pull_df_bigquery(
                     'portfoliooptimization-364417', 'assetclassprices', table,
                     'date')
+                price_data.index = pd.to_datetime(price_data.index)
                 price_data = price_data[['adjclose']]
                 price_data.columns = [table]
             else:
                 curr_price_data = gcp_engine.pull_df_bigquery(
                     'portfoliooptimization-364417', 'assetclassprices', table,
                     'date')
+                curr_price_data.index = pd.to_datetime(curr_price_data.index)
                 curr_price_data = curr_price_data[['adjclose']]
                 curr_price_data.columns = [table]
                 price_data = pd.concat([price_data, curr_price_data], axis=1)
