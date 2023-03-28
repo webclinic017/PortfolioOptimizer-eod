3dd8f94b6093e5260811be2000f9fea705814dd4
msquaredds <alex.melesko@msquaredds.com>
2023-03-28 12:43:50 -0400

    Getting user data

--- PortfolioOptimizer/Optimizer.py
+++ PortfolioOptimizer/Optimizer.py
@@ -10,21 +10,6 @@ from scipy.optimize import minimize
 from typing import Union
 
 
-def sharpe_ratio(weights: Union[list, np.ndarray],
-                 returns: Union[pd.DataFrame, np.ndarray]) -> float:
-    """
-    Calculate the Sharpe Ratio.
-    :param weights: The weights for the portfolio.
-    :param returns: The returns for the portfolio.
-    :return sharpe_ratio: The Sharpe Ratio.
-    """
-    avg = np.average(np.dot(weights, returns.T))
-    stddev = np.std(np.dot(weights, returns.T))
-    sharpe_ratio = avg / stddev
-
-    return sharpe_ratio
-
-
 def max_return(weights: Union[list, np.ndarray],
                returns: Union[pd.DataFrame, np.ndarray]) -> float:
     """
@@ -50,6 +35,18 @@ class Optimizer(object):
         """
         self.returns = returns
 
+    def sharpe_ratio(self, weights: Union[list, np.ndarray]) -> float:
+        """
+        Calculate the Sharpe Ratio.
+        :param weights: The weights for the portfolio.
+        :return sharpe_ratio: The Sharpe Ratio.
+        """
+        avg = np.average(np.dot(weights, self.returns.T))
+        stddev = np.std(np.dot(weights, self.returns.T))
+        sharpe_ratio = avg / stddev
+
+        return sharpe_ratio
+
     def optimize(self, method: str = 'sharpe_ratio') -> pd.DataFrame:
         """
         Run the optimization to get the weights for the portfolio.
@@ -68,7 +65,7 @@ class Optimizer(object):
 
         # get the objective function
         if method == 'sharpe_ratio':
-            func = sharpe_ratio
+            func = self.sharpe_ratio
         else:
             func = max_return
 
