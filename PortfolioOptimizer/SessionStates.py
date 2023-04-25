c7e9b40d0da5e0f48edd7d3dd5da4149b8d090c5
msquaredds <alex.melesko@msquaredds.com>
2023-04-25 12:20:31 -0400

    p

--- PortfolioOptimizer/SessionStates.py
+++ PortfolioOptimizer/SessionStates.py
@@ -16,16 +16,3 @@ def state_pull_ticker_tables():
         tables = data_engine.pull_ticker_tables()
         st.session_state.tables = tables
     return tables
-
-
-def state_pull_return_data(tables):
-    """Get the asset class return data."""
-    if 'return_data' in st.session_state and \
-            st.session_state.return_tables == tables:
-        return_data = st.session_state.return_data
-    else:
-        data_engine = DataTools()
-        return_data = data_engine.pull_return_data(tables)
-        st.session_state.return_data = return_data
-        st.session_state.return_tables = tables
-    return return_data
